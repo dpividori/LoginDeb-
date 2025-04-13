44697a5d04a037851137bbfe2d4df61038dc20ca
dpividori <dpividori@gmail.com>
2025-04-13 13:53:11 -0300

    V1

---
+++ Facultad/Facultad/Facultad/FormInicio.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Facultad
+{
+    partial class FormInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lll = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(128, 44);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(153, 199);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lll
+            // 
+            this.lll.AutoSize = true;
+            this.lll.Location = new System.Drawing.Point(53, 119);
+            this.lll.Name = "lll";
+            this.lll.Size = new System.Drawing.Size(61, 13);
+            this.lll.TabIndex = 5;
+            this.lll.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(128, 113);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // FormInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lll);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "FormInicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormInicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lll;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ Facultad/Facultad/Facultad/FormInicio.cs
@@ -0,0 +1,153 @@
+﻿using Facultad.Persistencia;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facultad
+{
+    public partial class FormInicio : Form
+    {
+        // partial class es una clase que se define en más de un archivo.
+        // en este caso FormInicio está heredando de la clase Form.
+        // PersistenciaUtils es una clase que maneja la persistencia de datos.
+        //persistenciaUtils  es un objeto de la clase PersistenciaUtils .
+        PersistenciaUtils persistenciaUtils = new PersistenciaUtils();
+        List<String> listado = new List<String>();
+        
+
+
+        public FormInicio()
+        {
+            InitializeComponent();
+            listado = persistenciaUtils.LeerRegistro("credenciales.csv");
+
+            // InitializeComponent es un método que inicializa los componentes de la clase FormInicio.
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            // 1) Validaciones
+
+            // 1.1) Validaciones de integridad de datos
+            // que usuario y password no estén vacíos
+
+            string Msg = "";
+
+            if (txtUsuario.Text == "")
+            {
+                Msg = "El campo usuario no puede estar vacío \n";
+            }
+            
+            if (txtPassword.Text == "")
+                    {
+                Msg = Msg + "El campo Password no puede estar vacío \n";
+
+            }
+
+
+            // 1.) Validaciones de negocio
+
+            // que el usuario exista en el archivo csv
+            // que el password exista en el archivo csv y se corresponda con el pass
+            // PersistenciaUtils
+
+            //public List<String> LeerRegistro(String nombreArchivo)
+            
+            //PUSE EL LISTADO EN UNA VARIABLE PARA PODER HACER LA VALIDACION
+
+            bool existeUsuario = false;
+            DateTime fechaUltimoCambio = DateTime.Now;
+            foreach (String linea in listado)
+            {
+                String[] columna = linea.Split(';');
+                // columna[0] = usuario
+                // columna[1] = password
+                  if (columna[0].Equals(txtUsuario.Text) && columna[1].Equals(txtPassword.Text))
+                  {
+                    existeUsuario = true;
+                    fechaUltimoCambio = DateTime.Parse(columna[2]);
+                    // si existe el usuario y el password, entonces se loguea
+                    // y se redirige al menu
+                    // FormMenu formMenu = new FormMenu();
+                    // formMenu.ShowDialog();
+                    break;
+                }
+            }
+
+            if (!existeUsuario)
+            {
+                Msg = Msg + "El usuario o password son incorrectos \n";
+                
+            }
+
+
+
+            // 1.1) Longitud de usuario (mayor igual a 6)
+
+            if (txtUsuario.Text.Length < 6)
+            {
+                Msg = Msg + "El campo usuario debe tener al menos 6 caracteres \n";
+            }
+
+
+            // 1.2) Longitud de password (mayor igual a 6)
+            if (txtPassword.Text.Length < 6)
+            {
+                Msg = Msg + "El campo password debe tener al menos 6 caracteres \n";
+            }
+
+            // 1.3) Primero Login? -> Cambio password
+            // 1.3) la primera vez que se loguea la una persona, crea usuario y password
+            if (txtPassword.Text.Equals("123456"))
+            {
+                Msg= Msg + "Debe actualizar su password \n";
+            }
+            
+            // 1.4) Expira password?
+            // cada 3 meses
+
+           if (fechaUltimoCambio.AddMonths(3) < DateTime.Now)
+            {
+                Msg = Msg + "Su password ha expirado, debe actualizarlo \n";
+            }
+
+
+            if (!string.IsNullOrEmpty(Msg)) 
+            {
+                MessageBox.Show(Msg);
+                return;
+            }
+
+            // 2) Redirigir
+            this.Hide();
+            FormMenu formMenu = new FormMenu();
+            formMenu.ShowDialog();
+        }
+
+        private List<String> obtenerUsuarios()
+        {
+            List<String> listado = persistenciaUtils.LeerRegistro("credenciales.csv");
+
+            return listado;
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormInicio_Load(object sender, EventArgs e)
+        {
+            txtUsuario.Text = "admin1";
+            txtPassword.Text = "Abc123";
+        }
+    }
+}
